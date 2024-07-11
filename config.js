fe39e3765fc7539e6f72595ecb04c288a40ac8a1
lirakH <lirakhamiti@hotmail.com>
2024-07-12 01:06:48 +0200

    name

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 // config.js
 
-export const MEDIA_BASE_URL = 'http://159.89.16.68:4000/';
+export const MEDIA_BASE_URL = 'https://159.89.16.68/';
 export const API_BASE_URL = `${MEDIA_BASE_URL}api/`;
