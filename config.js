01c5d44d2e7e7b45d30339447fa9877d60462d98
lirakH <lirakhamiti@hotmail.com>
2024-07-24 01:19:04 +0200

    drenas url

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 // config.js
 
-export const MEDIA_BASE_URL = 'https://e-ferizaj.online';
+//export const MEDIA_BASE_URL = 'https://e-ferizaj.online';
+export const MEDIA_BASE_URL = 'https://e-drenas.online';
 export const API_BASE_URL = `${MEDIA_BASE_URL}/api/`;
